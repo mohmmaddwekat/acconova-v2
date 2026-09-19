6d75033f930229d3ccfd2eaf0fa41a1bdd0b5d39
mohmmaddwekat <57283690+mohmmaddwekat@users.noreply.github.com>
2026-09-19 20:54:04 +0300

    feat(tasks): add task team model

---
+++ backend/app/Models/TaskTeam.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Concerns\BelongsToOrganization;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TaskTeam extends Model
+{
+    use BelongsToOrganization;
+    use SoftDeletes;
+
+    protected $guarded = [
+        'id',
+        'organization_id',
+    ];
+
+    public function department(): BelongsTo
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function leader(): BelongsTo
+    {
+        return $this->belongsTo(
+            StaffMember::class,
+            'leader_staff_member_id',
+        );
+    }
+
+    public function members(): BelongsToMany
+    {
+        return $this
+            ->belongsToMany(
+                StaffMember::class,
+                'task_team_members',
+                'task_team_id',
+                'staff_member_id',
+            )
+            ->withTimestamps();
+    }
+
+    public function projects(): BelongsToMany
+    {
+        return $this
+            ->belongsToMany(
+                TaskProject::class,
+                'task_team_projects',
+                'task_team_id',
+                'task_project_id',
+            )
+            ->withTimestamps();
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'capacity' => 'integer',
+        ];
+    }
+}
