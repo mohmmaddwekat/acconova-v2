f46c0ddbeceba0bed0a82876fcd6f2ed98d2f92d
mohmmaddwekat <57283690+mohmmaddwekat@users.noreply.github.com>
2026-09-21 20:21:02 +0300

    feat(auth): cast login timestamps

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -28,6 +28,8 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return [
             'email_verified_at' => 'datetime',
+            'previous_login_at' => 'datetime',
+            'last_login_at' => 'datetime',
 
             'password' => 'hashed',
         ];
